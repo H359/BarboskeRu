7c1b730a94a9e226ab23345e3e993611b4f98fc7
zw0rk <ostronom@gmail.com>
2011-12-01 03:28:56 +0400

    import && md

--- shop/management/commands/import_json.py
+++ shop/management/commands/import_json.py
@@ -6,6 +6,7 @@ import tidy
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
+from django.utils.encoding import smart_unicode
 from django.conf import settings
 from django.db import transaction
 
@@ -32,29 +33,32 @@ class Command(BaseCommand):
 	for p in range(len(path)):
 	    pslice = tuple(path[:p+1])
 	    if pslice not in self.categories:
-		self.categories[pslice] = Category.objects.create(title=pslice[-1], parent=category)
-	    #print (u'%s%s' % ('\t'*(len(pslice)-1), u'/'.join(pslice) ) ).encode('utf-8')
+		self.categories[pslice], created = Category.objects.get_or_create(title=pslice[-1], parent=category)
+		if created:
+		    print u'created %s (%s)' % (self.categories[pslice], category)
+		#print u'%s as child for %s' % (self.categories[pslice].title, category.title)
 	    category = self.categories[pslice]
 	return category
     def get_clean(self, html):
 	desc = u'<html><body>%s</body></html>' % html
 	desc = tidy.parseString(desc.encode('utf-8'), **self.tidy_opts)
 	self.doc.html = str(desc)
-	return self.doc.markdown
+	md = self.doc.markdown
+	return md
     def parse_product(self, obj):
 	if len(obj['articul'].strip()) == 0: return
+	category = self.get_or_create_category(obj['category'])
 	try:
-	    category = self.get_or_create_category(obj['category'])
-	    variant = Variant.objects.select_related().get(articul=obj['articul'])
+	    variant = Variant.objects.select_related().get(articul=obj['articul'].strip())
 	    ware = variant.ware
 	    ware.category = category
-	    ware.description = self.get_clean(obj['description'])
+	    ware.description.raw = smart_unicode(self.get_clean(''.join(obj['description'])))
 	    ware.save()
 	except ObjectDoesNotExist:
-	    pass
+	    print 'NO OBJ', obj['articul']
     def parse_category(self, obj):
 	category = self.get_or_create_category(obj['path'])
-	category.description = self.get_clean(obj['description'])
+	category.text.raw = smart_unicode(self.get_clean(u''.join(obj['description'])))
 	category.save()
     def handle(self, *args, **kwargs):
 	if len(args) < 1:
@@ -63,10 +67,12 @@ class Command(BaseCommand):
 	if not os.path.exists(args[0]):
 	    print u'%s not found.' % args[0]
 	    return
-	with open(args[0], 'r') as jsonfile:
-	    data = ujson.load(jsonfile)
-	    for obj in data:
-		if obj['type'] == 'product':
-		    self.parse_product(obj)
-		else:
-		    self.parse_category(obj)
+	with transaction.commit_on_success():
+	    with open(args[0], 'r') as jsonfile:
+		data = ujson.load(jsonfile)
+		for obj in data:
+		    if obj['type'] == 'product':
+			self.parse_product(obj)
+		    else:
+			self.parse_category(obj)
+	#Category.objects.rebuild()
